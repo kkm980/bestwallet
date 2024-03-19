88c396e0f42fa8e0abe4e760bd7a664ff96fe075
kkm980 <kantkrishna751@gmail.com>
2024-03-19 17:20:17 +0530

    added fevicon and meta

--- app/about/layout.tsx
+++ app/about/layout.tsx
@@ -12,6 +12,15 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="icon" type="image/png" href="/one.png" /> {/* Change href to your favicon image */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Your Twitter Title" /> {/* Change to your Twitter title */}
+        <meta name="twitter:description" content="Your Twitter Description" /> {/* Change to your Twitter description */}
+        <meta name="twitter:image" content="/twitter-image.jpg" /> {/* Change to the URL of your Twitter image */}
+      </head>
       <body>{children}</body>
     </html>
   );
