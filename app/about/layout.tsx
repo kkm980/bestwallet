e28e3759d4cfb686660c355a559674dcdf701b57
kkm980 <kantkrishna751@gmail.com>
2024-03-19 17:44:06 +0530

    added twitter desc and title

--- app/about/layout.tsx
+++ app/about/layout.tsx
@@ -20,8 +20,8 @@ export default function RootLayout({
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="icon" type="image/png" href="/two.png" /> {/* Change href to your favicon image */}
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content="Your Twitter Title" /> {/* Change to your Twitter title */}
-        <meta name="twitter:description" content="Your Twitter Description" /> {/* Change to your Twitter description */}
+        <meta name="twitter:title" content="About twitter title" /> {/* Change to your Twitter title */}
+        <meta name="twitter:description" content="About twitter desc" /> {/* Change to your Twitter description */}
         <meta name="twitter:image" content="/twitter-image.jpg" /> {/* Change to the URL of your Twitter image */}
       </head>
       <body>{children}</body>
--- app/secondary/layout.tsx
+++ app/secondary/layout.tsx
@@ -20,8 +20,8 @@ export default function RootLayout({
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="icon" type="image/png" href="/one.png" /> {/* Change href to your favicon image */}
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content="Your Twitter Title" /> {/* Change to your Twitter title */}
-        <meta name="twitter:description" content="Your Twitter Description" /> {/* Change to your Twitter description */}
+        <meta name="twitter:title" content="Secondary title twitter" /> {/* Change to your Twitter title */}
+        <meta name="twitter:description" content="secondary desc twitter" /> {/* Change to your Twitter description */}
         <meta name="twitter:image" content="/twitter-image.jpg" /> {/* Change to the URL of your Twitter image */}
       </head>
       <body>{children}</body>
