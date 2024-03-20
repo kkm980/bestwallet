2b0a5b24f1dfe589cb2d0c3700cbc147654c0d20
kkm980 <kantkrishna751@gmail.com>
2024-03-20 12:26:00 +0530

    updated code

--- app/lander-page/layout.tsx
+++ app/lander-page/layout.tsx
@@ -1,5 +1,7 @@
 "use client";
-
+import { useEffect, useState } from "react";
+import { useQuery } from "@apollo/client";
+import { DAILY_QUEST_TASKS } from "../../GraphQL/Queries";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const queryClient = new QueryClient();
@@ -9,6 +11,29 @@ export default function LanderPageLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const apiKey = process.env.REACT_APP_LEADERBOARD_API_KEY;
+  const walletAddress = localStorage.getItem("bestWalletData");
+  const variables = {
+    activityTrackerId: process.env.REACT_APP_ACTIVITY_TRACKER_ID,
+    walletAddress,
+    active: true,
+  };
+  const { data } = useQuery(DAILY_QUEST_TASKS, {
+    variables: {
+      ...variables,
+    },
+    context: {
+      headers: {
+        "x-api-key": apiKey,
+      },
+    },
+    skip: !walletAddress, // Skip the query when userWalletAddress is null
+  });
+
+  useEffect(() => {
+    
+  }, [data]);
+
   return (
     <QueryClientProvider client={queryClient}>
       <html lang="en">
